033d7f13a997289f54cad8467c90d15ee3f0f119
Evan Xu <Evan Xu>
2016-10-31 22:09:24 +0800

    add mongoose at mongo://localhost/gtask

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+'use strict';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('./config/express');
+
+var mongoose = require('./config/mongoose')
+  , express = require('./config/express');
+
+var db = mongoose();
 var app = express();
 
 app.listen(3000);
