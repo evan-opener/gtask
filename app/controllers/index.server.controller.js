c58db234f99614d1c4b9bde061c3d84c84d4dede
XU Yan (810312) <810312@ft.grp>
2016-10-31 16:01:40 +0800

    session worked

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,11 @@
 exports.render = function(req, res) {
+  // visit session
+  if (req.session.lastVisit) {
+    console.log(req.session.lastVisit);
+  }
+
+  req.session.lastVisit = new Date();
+
   res.render('index', {
     title: 'gTask'
   });
