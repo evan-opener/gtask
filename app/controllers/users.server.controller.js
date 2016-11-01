c535c5b97900ce5365c035716bb4896ec0f16e91
XU Yan (810312) <810312@ft.grp>
2016-11-01 17:01:09 +0800

    add user update api

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -44,6 +44,15 @@ exports.userByID = function(req, res, next, id) {
   });
 };
 
+exports.update = function(req, res, next) {
+  User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(user);
+    }
+  });
+};
 // User special find
 /*
 exports.findName = function(req, res, next) {
