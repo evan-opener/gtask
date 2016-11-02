cfc0e57363b213cdab004a3a8fea64fc5a20493c
XU Yan (810312) <810312@ft.grp>
2016-11-02 09:11:36 +0800

    add user delete api

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -53,6 +53,16 @@ exports.update = function(req, res, next) {
     }
   });
 };
+
+exports.delete = function(req, res, next) {
+  req.user.remove(function(err) {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(req.user);
+    }
+  });
+};
 // User special find
 /*
 exports.findName = function(req, res, next) {
