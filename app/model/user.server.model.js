1724ef8de8ffb6f3ed80be0f78b125843512e305
XU Yan (810312) <810312@ft.grp>
2016-11-02 10:23:10 +0800

    add validator at user schema

--- app/model/user.server.model.js
+++ app/model/user.server.model.js
@@ -1,16 +1,65 @@
 'use strict';
 
 // invoke mongoose and schema
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    validator = require('validator');
+
+// A validation function for local stragtegy properties
+var validateLocalStrategyProperty = function (property) {
+  return ((this.provider !== 'local' && !this.updated) || property.length);
+};
+
+// A validation function for local strategy email
+var validateLocalStrategyEmail = function (email) {
+  return ((this.provider !== 'local' && !this.updated) || validator.isEmail(email));
+};
 
 // Define user schema
 var UserSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  username: String,
-  password: String
+  firstName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your last name']
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
+  },
+  username: {
+    type: String,
+    unique: 'Username already exists',
+    required: 'Please fill in a username',
+    lowercase: true,
+    trim: true
+  },
+  password: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+// add virtual attributes for full name
+UserSchema.virtual('fullName').get(function () {
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
