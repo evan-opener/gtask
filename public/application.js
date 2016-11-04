7a17183d06dd6191765c897e066382cd502c648e
EvanXu on Mac <evan.xu0723@gmail.com>
2016-11-04 22:18:14 +0800

    config the ngRoute module

--- public/application.js
+++ public/application.js
@@ -1,9 +1,15 @@
+'use strict';
 // Define the main mdoule name for this app
 var mainApplicationModuleName = 'gtask';
 
 // Use module method to create main application
 // The other module can be inject as dependencies
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'nexample']);
+
+// Make the route have the right url as /#!/xxx
+mainApplicationModule.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('!');
+}]);
 
 // Use angular.bootstrap method to initiate a new AngularJS application use the main application module.
 angular.element(document).ready(function() {
